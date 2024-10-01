498c2a4199ffa141f747299c7f7d458f17908821
kderrlo <derrylzodxyt_gmail.com>
2024-10-01 07:56:21 +0700

    ~

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,5 @@
-import altair as alt
-import numpy as np
-import pandas as pd
-import streamlit as st
 from menu import show_menu
-import pickle
-import sklearn
-from sklearn.preprocessing import Normalizer
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-from sklearn.pipeline import Pipeline
-import joblib
-import matplotlib.pyplot as plt
-from streamlit_option_menu import option_menu
-
-        
 def main():
-        # random_button_callback(True)
-    # main_render()
     show_menu()
     return 0
 
